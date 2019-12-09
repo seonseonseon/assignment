a38abddc28cd4670b4f43b3160d800ec8ded0dd3
Seon <tjsghdu39@gmail.com>
2019-12-09 23:34:03 +0900

    third commit

--- 20186628_07.c
+++ 20186628_07.c
@@ -2,25 +2,30 @@
 // 20186628 임선호
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-typedef struct node
+//#define _CRT_SECURE_NO_WARNINGS
+
+typedef struct nodeStruct
 {
 	int tag; //
-	char* value;
+	char* value; //
 	int count;
 }node;
 
 char* casedown(char* input);
 void search_insert(char* target);
-void printAll();
+void printAll(FILE* write);
 
 // argv[0] input.txt
 // argv[1] output.txt
 
-node nodes[1200000]; //1116092개 정도의 토큰이 있는듯
+node nodes[1300000]; //1116092개 정도의 토큰이 있는듯
 int main(int argc, char* argv[]) {
-	nodes[0].tag = NULL;
+	for (int i = 0; i < 1300000; i++) {
+		nodes[i].tag = 0;
+	}
 
 	//file
 	FILE *read, *write;
@@ -41,9 +46,10 @@ int main(int argc, char* argv[]) {
 	char str[1000];	//평균 한 라인에 200자가 넘지 않는 걸로 보임...
 	int toknum = 0;
 	
-	const char delimiters[] = ", !.?\'\"#*-_=[]():~^{}><@/&$%`;|\\\n";	//'s 를 포함할까
+	const char delimiters[] = ", !.?\'\"#*-_=+[]():~^{}><@/&$%`;|\\\n\t\s";	//'s 를 포함할까
 	
-	while(fgets(str, sizeof(str), read) != NULL){
+
+	while (fgets(str, sizeof(str), read) != NULL) {
 		char *token = strtok(str, delimiters);
 
 		while (token != NULL)
@@ -51,36 +57,20 @@ int main(int argc, char* argv[]) {
 			//대소문자 변경
 			token = casedown(token);
 
-			printf("[%07d] %s\n", ++toknum, token); //
-
-			//이미 있는지 체크하여 token을 저장하거나 count++하는 함수
-			//int i = 0;
 			search_insert(token);
-			/*
-			if (tempnode.tag == NULL) {
-				nodes[i].value = token;
-				nodes[i].count = 1;
-				nodes[i].tag = 1;
-				nodes[i + 1].tag = NULL;
-				i++;
-			}*/
 			
+			printf("[%07d] %s\n", ++toknum, token); //
 
 			//token을 저장 
 			//temp[toknum] = token;
-
 			token = strtok(NULL, delimiters);
 			//NULL을 넣으면 방금전에 분리한 문자열의 남은 문자열을 가지고 다시
 		}
-
 	}
 
 
-	
-
-
 	//output file write
-	printAll();
+	printAll(write); //
 	printf("\n");
 	fprintf(write, "\n");
 
@@ -118,41 +108,38 @@ char* casedown(char* input) {
 
 //Null 나오면 새로 insert
 //search 하여 나온 노드 count++;
-void search_insert(char* target) {
+void search_insert(char *target) {
 	int i = 0;
-	while (nodes[i].tag != NULL) {
+	while (nodes[i].tag != 0) {
 		if (strcmp(nodes[i].value, target) == 0) {
 			nodes[i].count++;
 			return;
 		}
-
-		i++;
+		else
+			i++;
 	}
-	nodes[i].value = target;
+
+	char* tmp = (char*)malloc(sizeof(char) * 100); //
+	memcpy(tmp, target, strlen(target) + 1);
+
+	nodes[i].value = tmp;
+	//strcpy_s(nodes[i].value, sizeof(target) - 1 , target);
+	//nodes[i].value[99] = NULL;
 	nodes[i].count = 1;
 	nodes[i].tag = 1;
-	nodes[i + 1].tag = NULL;
-
 	return;
 }
 
-void printAll()
+void printAll(FILE * write)
 {
-	if (nodes[0].tag == NULL)
+	if (nodes[0].tag == 0)
 		return;
+
 	int i = 0;
-	while (nodes[i].tag != NULL) {
-		printf("[%07d]%s  (%d)\n",i, nodes[i].value, nodes[i].count); //
-		fprintf("[%07d]%s  (%d)\n",i, nodes[i].value, nodes[i].count);
+	while (nodes[i].tag != 0) {
+		printf("[%07d]%s (%d)\n",i + 1, nodes[i].value, nodes[i].count); //
+		fprintf(write, "[%07d]%s (%d)\n",i + 1, nodes[i].value, nodes[i].count);
 		i++;
 	}
 	return;
 }
-
-/*
-for (int i = 0; i < strlen(result); i++) {
-char ch = result;
-if ('A' <= ch && ch <= 'Z') {
-ch = ch + 32;
-}
-}*/
